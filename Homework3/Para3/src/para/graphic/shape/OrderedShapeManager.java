322065ec9a54dd6f343affc93106d3aa311d3d6a
ri.b.aa <ri.b.aa@cs-mac79.edu.c.titech.ac.jp>
2018-07-06 09:15:17 +0900

    nothing

--- Homework3/Para3/src/para/graphic/shape/OrderedShapeManager.java
+++ Homework3/Para3/src/para/graphic/shape/OrderedShapeManager.java
@@ -6,9 +6,10 @@ import para.graphic.target.Target;
 /** 描画順序をidの順とする図形集合
  */
 public class OrderedShapeManager extends ShapeManager{
-	final AbstractCollection<Shape> data;
+//	final AbstractCollection<Shape> data;
 	public OrderedShapeManager() {
-		data = TreeSet<Shape>();
+//		data = TreeSet<Shape>();
+		super();
 	}
 	
 	  /** 図形の集合を生成する（空集合）.
@@ -19,13 +20,13 @@ public class OrderedShapeManager extends ShapeManager{
 		  super(data);
 	  }
 	  
-//	  @Override
-//	  /** 集合内の図形を出力する．
-//	   *  @param target 出力装置
-//	   */
-//	  public synchronized void draw(Target target){
-//		  data.stream().sorted((s1,s2)->s1.getID()-s2.getID()).forEach(s->s.draw(target));
-//	  }
+	  @Override
+	  /** 集合内の図形を出力する．
+	   *  @param target 出力装置
+	   */
+	  public synchronized void draw(Target target){
+		  data.stream().sorted(((s1,s2)->s1.getID()-s2.getID()).forEach(s->s.draw(target));
+	  }
 
 
 
