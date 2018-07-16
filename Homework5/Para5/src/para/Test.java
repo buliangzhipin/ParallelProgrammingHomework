76615166411c5c34b58fd46024222d10592ace8f
buliangzhipin <34953134+buliangzhipin@users.noreply.github.com>
2018-07-16 21:39:51 +0800

    1111

---
+++ Homework5/Para5/src/para/Test.java
@@ -0,0 +1,43 @@
+package para;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+
+public class Test {
+	public static void main(String[] args) {
+		File file = new File("1.jpg");
+		File file2 = new File("2.jpg");
+		
+		try(OutputStream out = new FileOutputStream(file2);InputStream rd = new FileInputStream(file)) {
+			BufferedInputStream bin = new BufferedInputStream(rd);
+			BufferedOutputStream bout = new BufferedOutputStream(out);
+			
+			byte[] b = new byte[1024];
+			while(bin.read(b)!=-1) {
+				bout.write(b);
+			}
+			bin.close();
+			bout.close();
+			System.out.println("finished");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		
+	}
+}
